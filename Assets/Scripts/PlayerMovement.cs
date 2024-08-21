11b47982bb695c59ac485acdfd4a7611e441bcdf
Qriscuit <hursh.nashine1998@gmail.com>
2024-08-21 15:06:11 -0600

    Boundaries added for the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
     public float _Speed = 10f;
     public GameObject ShootingPoint;
+    public float LeftBoundary, RightBoundary;
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
+            if (transform.position.x < LeftBoundary) return;
             transform.Translate(new Vector3(-_Speed * Time.deltaTime, 0, 0));
         }
 
         if (Input.GetKey(KeyCode.D))
         {
+            if (transform.position.x > RightBoundary) return;
             transform.Translate(new Vector3(_Speed * Time.deltaTime, 0, 0));
         }
     }

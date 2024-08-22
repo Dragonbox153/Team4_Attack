f4df2805286ee0dc74d7b415033376462039df48
Qriscuit <hursh.nashine1998@gmail.com>
2024-08-22 16:31:31 -0600

    some fixes to enemiss going up and down

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -24,6 +24,7 @@ public class GameManager : MonoBehaviour
     //what the current tide level is
     public float CurrentTideLevel = -2.5f;
 
+    public bool _transitioning = false;
 
     Vector3 BufferPosition = Vector3.zero; 
 
@@ -34,9 +35,16 @@ public class GameManager : MonoBehaviour
 
     //Singleton
     public static GameManager Instance;
+
+    float TopEdge;
+    float BotEdge;
     private void Awake()
     {
         Instance = this;
+
+        TopEdge = Camera.main.ViewportToWorldPoint(new Vector3(0, 1f, Camera.main.nearClipPlane)).y - 0.3f;
+        BotEdge = Camera.main.ViewportToWorldPoint(new Vector3(0, 0f, Camera.main.nearClipPlane)).y + 0.3f;
+
     }
 
     private void Update()
@@ -47,23 +55,30 @@ public class GameManager : MonoBehaviour
 
         BGSpriteRenderer.transform.eulerAngles = new Vector3(0,0,DayNightCyclePNG_angle);
 
+        
+
         if(Mathf.Ceil(DayNightCyclePNG_angle) == 90)
         {
+            Debug.Log("90");
             StartCoroutine(ChangeTide(LowTide, MidTide));
         }
 
         if (Mathf.Ceil(DayNightCyclePNG_angle) == 180)
         {
+            Debug.Log("180");
             StartCoroutine(ChangeTide(MidTide, HighTide));
         }
 
         if (Mathf.Ceil(DayNightCyclePNG_angle) == 270)
         {
+            Debug.Log("270");
             StartCoroutine(ChangeTide(HighTide, MidTide));
         }
 
         if (Mathf.Ceil(DayNightCyclePNG_angle) == 360)
         {
+            Debug.Log("360");
+
             DayNightCyclePNG_angle = 0;
             StartCoroutine(ChangeTide(MidTide, LowTide));
         }
@@ -74,19 +89,39 @@ public class GameManager : MonoBehaviour
     IEnumerator ChangeTide(Vector3 A, Vector3 B)
     {
         float tidechangeElapsedTime = 0;
+        float previousBufferVal = 0;
+        _transitioning = true;
 
         while (tidechangeElapsedTime < TideChangeDuration)
         {
             float t = tidechangeElapsedTime / TideChangeDuration;
             BufferPosition = Vector3.Lerp(A, B, t);
+            
+            float DeltaBufferY = BufferPosition.y - previousBufferVal;
+            
             Player.transform.position = new Vector3(Player.transform.position.x, BufferPosition.y, 0);
+
+            //Debug.Log(DeltaBufferY);
+
+            foreach (GameObject enemy in EnemySpawner.instance.liveEnemies)
+            {
+                if (enemy.transform.position.y > TopEdge) continue;
+                else if (enemy.transform.position.y < BotEdge) continue;
+                enemy.transform.position += new Vector3(0, DeltaBufferY, 0);
+            }
+
             CurrentTideLevel = Player.transform.position.y;
             tidechangeElapsedTime += Time.deltaTime;
+
+            previousBufferVal = BufferPosition.y;
+
             yield return null;
         }
 
         Player.transform.position = new Vector3(Player.transform.position.x, B.y, 0);
         CurrentTideLevel = B.y;
+
+        _transitioning = false;
     }
 
     
